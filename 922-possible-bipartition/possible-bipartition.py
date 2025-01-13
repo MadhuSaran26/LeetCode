8edf4874c006ccf153b20a2cd36dcba7cdcdec96
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-12 23:42:34 -0700

    Time: 37 ms (55.48%) | Memory: 21.4 MB (63.28%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.py
+++ 922-possible-bipartition/possible-bipartition.py
@@ -6,39 +6,26 @@ class Solution:
             adj_list[u].append(v)
             adj_list[v].append(u)
         
-        group1, group2 = set(), set()
+        color = dict()
         visited = set()
 
         def bfs(idx):
             queue = deque([idx])
+            color[idx] = 1
 
             while queue:
                 node = queue.popleft()
-                if node in visited:
-                    continue
-                visited.add(node)
                 for neighbor in adj_list[node]:
-                    if node in group1:
-                        if neighbor in group1:
-                            return False
-                        if neighbor in group2:
-                            continue
-                        group2.add(neighbor)
-                    elif node in group2:
-                        if neighbor in group2:
-                            return False
-                        if neighbor in group1:
-                            continue
-                        group1.add(neighbor)
-                    else:
-                        group1.add(node)
-                        group2.add(neighbor)
-                    if neighbor not in visited:
+                    if neighbor not in color:
+                        color[neighbor] = 1 - color[node]
                         queue.append(neighbor)
+                    elif color[neighbor] == color[node]:
+                        return False
             return True  
         
         for idx in range(1, n+1):
-            if idx not in visited:
+            if idx not in color:
                 if not bfs(idx):
                     return False 
+        
         return True  
