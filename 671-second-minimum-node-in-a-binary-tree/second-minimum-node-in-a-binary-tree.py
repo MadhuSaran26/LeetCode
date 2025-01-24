8ab155c68e2ec1a961693ab77c94d5d7ea8c5550
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-24 15:40:22 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (7.31%) - LeetSync

---
+++ 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findSecondMinimumValue(self, root: Optional[TreeNode]) -> int:
+        if not root.left and not root.right:
+            return -1
+        
+        second = float('inf')
+        
+        def dfs(node):
+            nonlocal second
+            if not node:
+                return
+            if node.val > second:
+                return
+            if node.val != root.val and node.val < second:
+                second = node.val
+            dfs(node.left)
+            dfs(node.right)
+        
+        dfs(root)
+        return second if second != float('inf') else -1
+            
+        
