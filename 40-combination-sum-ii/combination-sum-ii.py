c823688fada78a9c9418689233038feda6468085
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-25 10:33:38 -0700

    Time: 3 ms (93.38%) | Memory: 18.1 MB (10.43%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        candidates.sort() # sorting helps in handling duplicates in candidates
+
+        def backtracking(current, remaining, start):
+            if remaining == 0 and current not in result:
+                result.append(list(current))
+                return
+            
+            if remaining < 0:
+                return
+            
+            for idx in range(start, len(candidates)):
+                if idx > start and candidates[idx] == candidates[idx-1]:
+                    continue
+                # stop when the left out elements in candidates are higher than "remaining"
+                if candidates[idx] > remaining:
+                    break
+                current.append(candidates[idx])
+                backtracking(current, remaining - candidates[idx], idx+1)
+                current.pop()
+            
+        
+        backtracking([], target, 0)
+        return result
+        
