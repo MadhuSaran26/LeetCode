3a95c144b784f81953ba93a4326cc92824232802
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-24 19:21:22 -0700

    Time: 3 ms (22.34%) | Memory: 17.7 MB (57.79%) - LeetSync

--- 961-long-pressed-name/long-pressed-name.py
+++ 961-long-pressed-name/long-pressed-name.py
@@ -2,26 +2,15 @@ class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
         npointer = tpointer = 0
         nlen, tlen = len(name), len(typed)
-        ncntr, tcntr = Counter(name), Counter(typed)
-        if len(ncntr) != len(tcntr):
-            return False
-        for char in ncntr:
-            if ncntr[char] > tcntr[char]:
-                return False
-        while npointer < nlen and tpointer < tlen:
-            if name[npointer] != typed[tpointer]:
-                return False
-            else:
-                char = name[npointer]
-                while npointer < nlen and tpointer < tlen and name[npointer] == typed[tpointer] and name[npointer] == char:
-                    npointer += 1
+        while tpointer < tlen:
+            if npointer < nlen and name[npointer] == typed[tpointer]:
+                npointer += 1
+                tpointer += 1
+                if  0 < npointer < nlen and tpointer < tlen and name[npointer] == name[npointer-1] and name[npointer] == typed[tpointer]:
+                    continue
+                while 0 < tpointer < tlen and typed[tpointer] == typed[tpointer-1]:
                     tpointer += 1
-                if npointer < nlen and name[npointer] != char:
-                    while tpointer < tlen and typed[tpointer] == char:
-                        tpointer += 1
-                elif npointer == nlen and tpointer < tlen and typed[tpointer] == char:
-                    while tpointer < tlen and typed[tpointer] == char:
-                        tpointer += 1
-                elif npointer < nlen:
-                    return False
-        return npointer == nlen and tpointer == tlen
+            else:
+                return False
+
+        return npointer == nlen
