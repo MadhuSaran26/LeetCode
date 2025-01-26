d895ca3625bfba2e913776e2a5daa234a64c5bbb
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-26 14:16:44 -0700

    Time: 247 ms (36.07%) | Memory: 18.9 MB (27.50%) - LeetSync

---
+++ 254-factor-combinations/factor-combinations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getFactors(self, n: int) -> List[List[int]]:
+        result = []
+        if n == 1:
+            return result
+
+        def backtracking(current, remaining, start):
+            if len(current)>0:
+                result.append(current+[remaining])
+            
+            for idx in range(start, int(remaining**0.5)+1):
+                print(idx, start, int(remaining**0.5)+1)
+                if remaining % idx == 0:
+                    backtracking(current+[idx], remaining//idx, idx)
+        
+        backtracking([], n, 2)
+        return result
+        
