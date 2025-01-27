73c885895cf55b3440e2a7e650cb5af9ce869173
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-27 14:57:18 -0700

    Time: 2545 ms (5.01%) | Memory: 19.5 MB (7.04%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -7,20 +7,19 @@ class Solution:
         min_window = float('inf')
         window_cnt = defaultdict(int)
         result = []
-        have, need = 0, len(t)
+        have, need = 0, len(tcnt)
         for right in range(slen):
-            if s[right] in tcnt and window_cnt[s[right]] < tcnt[s[right]]:
-                have += 1
             window_cnt[s[right]] += 1
-
+            if s[right] in tcnt and window_cnt[s[right]] == tcnt[s[right]]:
+                have += 1
             while have == need:
                 if min_window > right-left+1:
                     min_window = right-left+1
                     result = s[left:right+1]
                 
-                if s[left] in tcnt and window_cnt[s[left]] == tcnt[s[left]]:
-                    have -= 1
                 window_cnt[s[left]] -= 1
+                if s[left] in tcnt and window_cnt[s[left]] < tcnt[s[left]]:
+                    have -= 1
                 left += 1
 
         return ''.join(result)
