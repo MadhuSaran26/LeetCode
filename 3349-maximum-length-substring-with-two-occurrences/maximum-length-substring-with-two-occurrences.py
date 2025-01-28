61dd4a523c42d659d1ae8c04b108a6970458ae58
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-28 16:40:13 -0700

    Time: 9 ms (30.74%) | Memory: 17.5 MB (66.84%) - LeetSync

---
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        result = float('-inf')
+        left = 0
+        seen = defaultdict(int)
+        for right in range(len(s)):
+            while seen.get(s[right], 0)==2:
+                seen[s[left]] -= 1
+                left += 1
+            seen[s[right]] += 1
+            result = max(result, right-left+1)
+        return result
+
+
+
+        
