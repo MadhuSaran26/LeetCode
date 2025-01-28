8d817fbedb0896d402543c389d673766f8ebcc28
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-28 11:45:50 -0700

    Time: 27 ms (48.01%) | Memory: 27.1 MB (93.76%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -6,7 +6,7 @@ class Solution:
         p = 10**9+7 # large prime modulus
         b = 4 # base - 4 characters in DNA sequence
         b_multiplier = pow(b, p_len - 1, p) # Precompute base multiplier for removing old character
-        hash_dict = defaultdict(int)
+        seen = set()
         char2int = {'A':0, 'C':1, 'G':2, 'T':3} # character mapping for hashing
         p_hash = 0
         result = set()
@@ -14,7 +14,7 @@ class Solution:
         for idx in range(p_len):
             p_hash = (p_hash * b + char2int[s[idx]]) % p
         
-        hash_dict[p_hash] += 1
+        seen.add(p_hash)
         
         for idx in range(1, len(s)-p_len+1):
             old, new = s[idx-1], s[idx+p_len-1]
@@ -24,9 +24,9 @@ class Solution:
             # adding new character's contribution
             #p_hash = (p_hash * b + char2int[new]) % p
             p_hash = ((p_hash - char2int[old] * b_multiplier) * b + char2int[new]) % p
-            hash_dict[p_hash] += 1
-            if hash_dict[p_hash] == 2:
+            if p_hash in seen:
                 result.add(s[idx:idx+p_len])
+            seen.add(p_hash)
         
         return list(result)
 
