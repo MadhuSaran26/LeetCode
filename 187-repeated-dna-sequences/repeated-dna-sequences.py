7e713519e0d787205388676bd2242f3108eb71e4
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-28 10:45:19 -0700

    Time: 39 ms (16.70%) | Memory: 29.1 MB (37.98%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        p_len = 10 # pattern length
+        if len(s) < 10:
+            return []
+        p = 10**9+7 # large prime modulus
+        b = 4 # base - 4 characters in DNA sequence
+        b_multiplier = pow(b, p_len - 1, p) # Precompute base multiplier for removing old character
+        hash_dict = defaultdict(int)
+        char2int = {'A':0, 'C':1, 'G':2, 'T':3} # character mapping for hashing
+        p_hash = 0
+        s = list(s)
+        result = set()
+        # calculating hash
+        for idx in range(p_len):
+            p_hash = (p_hash * b + char2int[s[idx]]) % p
+        
+        hash_dict[p_hash] += 1
+        
+        for idx in range(1, len(s)-p_len+1):
+            old, new = s[idx-1], s[idx+p_len-1]
+            # Update the rolling hash
+            # removing old character's contribution
+            p_hash = (p_hash - char2int[old] * b_multiplier) % p
+            # adding new character's contribution
+            p_hash = (p_hash * b + char2int[new]) % p
+            if p_hash < 0:  # Ensure non-negative hash
+                p_hash += p
+            hash_dict[p_hash] += 1
+            if hash_dict[p_hash] > 1:
+                result.add(''.join(s[idx:idx+p_len]))
+        
+        return list(result)
+
+
+
+            
+        
