631311d9554f7291b488a405805bceb80002a1c4
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-28 22:28:40 -0700

    Time: 4415 ms (35.16%) | Memory: 18.1 MB (22.66%) - LeetSync

---
+++ 3422-find-the-n-th-value-after-k-seconds/find-the-n-th-value-after-k-seconds.py
@@ -0,0 +1,10 @@
+class Solution:
+    def valueAfterKSeconds(self, n: int, k: int) -> int:
+        MOD = 10**9+7
+        arr = [1]*n
+        for _ in range(k):
+            for idx in range(1, n):
+                arr[idx] += arr[idx-1]
+        
+        return arr[n-1] % MOD
+        
