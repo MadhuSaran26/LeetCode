097be32a37177fb2a4d86a2f34b1f549988ca3ba
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-28 22:33:48 -0700

    Time: 2965 ms (77.34%) | Memory: 17.6 MB (74.22%) - LeetSync

--- 3422-find-the-n-th-value-after-k-seconds/find-the-n-th-value-after-k-seconds.py
+++ 3422-find-the-n-th-value-after-k-seconds/find-the-n-th-value-after-k-seconds.py
@@ -3,9 +3,12 @@ class Solution:
         MOD = 10**9+7
         arr = [1]*n
         for _ in range(k):
-            for idx in range(1, n):
-                arr[idx] += arr[idx-1]
-                #arr[idx] %= MOD
+            prefix_sum = []
+            curr_sum = 0
+            for num in arr:
+                curr_sum += num
+                prefix_sum.append(curr_sum)
+            arr = prefix_sum
         
         return arr[n-1] % MOD
         
