27a569e216ac57ac09f62abbc8ea9ae719c185ea
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-28 17:57:36 -0700

    Time: 114 ms (78.36%) | Memory: 26.1 MB (44.03%) - LeetSync

--- 1359-circular-permutation-in-binary-representation/circular-permutation-in-binary-representation.py
+++ 1359-circular-permutation-in-binary-representation/circular-permutation-in-binary-representation.py
@@ -1,6 +1,8 @@
 class Solution:
     def circularPermutation(self, n: int, start: int) -> List[int]:
-        gray_code = [i ^ (i >> 1) for i in range(2**n)]
+        # (i>>1) equivalent to diving i by 2^1, right shift is division
+        # (1<<n) equivalent to multiplying 1 by 2^n, left shoft is multiplication
+        gray_code = [i ^ (i >> 1) for i in range(1<<n)]
         start_idx = gray_code.index(start)
         
         return gray_code[start_idx:] + gray_code[:start_idx]
