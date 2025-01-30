51ad2e5a7bd97b063050858291a71a7479d71ca4
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-01-29 22:08:14 -0700

    Time: 56 ms (83.50%) | Memory: 34.6 MB (25.81%) - LeetSync

--- 432-all-oone-data-structure/all-oone-data-structure.py
+++ 432-all-oone-data-structure/all-oone-data-structure.py
@@ -1,60 +1,87 @@
+class Node:
+    def __init__(self, freq):
+        self.freq = freq
+        self.prev = None
+        self.next = None
+        self.keys = set()
+
 class AllOne:
 
     def __init__(self):
-        self.prev_operation = 0
-        self.result = ""
         self.data = dict()
-        
+        self.head = Node(0)
+        self.tail = Node(0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def inc(self, key: str) -> None:
-        self.prev_operation = 0
         if key in self.data:
-            self.data[key] += 1
+            freq_node = self.data[key]
+            freq = freq_node.freq
+            freq_node.keys.remove(key)
         else:
-            self.data[key] = 1
+            freq = 0
+            freq_node = self.head
+        
+        next_node = freq_node.next
+        if next_node == self.tail or next_node.freq != freq+1:
+            new_node = Node(freq+1)
+            new_node.keys.add(key)
+            new_node.prev = freq_node
+            new_node.next = next_node
+            next_node.prev = new_node
+            freq_node.next = new_node 
+            self.data[key] = new_node
+        else:
+            next_node.keys.add(key)
+            self.data[key] = next_node
+        
+        if freq_node != self.head and not freq_node.keys:
+            self.removeNode(freq_node)
+        
         return None
 
     def dec(self, key: str) -> None:
-        self.prev_operation = 0
-        self.data[key] -= 1
-        if self.data[key] == 0:
-            del self.data[key]  
-        return None
+        freq_node = self.data[key]
+        freq_node.keys.remove(key)
+        freq = freq_node.freq - 1
 
-    def getMaxKey(self) -> str:
-        if self.prev_operation == 1:
-            return self.result
-        self.prev_operation = 1
-        if self.data:
-            count = list(self.data.values())[0]
-            key = list(self.data.keys())[0]
-            for key_d in self.data:
-                if self.data[key_d] > count:
-                    count = self.data[key_d]
-                    key = key_d
-            self.result = key
-            return key
+        prev_node = freq_node.prev
+        if freq == 0:
+            del self.data[key]
+        elif prev_node == self.head or prev_node.freq != freq:
+            new_node = Node(freq)
+            new_node.keys.add(key)
+            new_node.prev = prev_node
+            new_node.next = freq_node
+            prev_node.next = new_node
+            freq_node.prev = new_node
+            self.data[key] = new_node
         else:
-            self.result = ""
-            return ""
-        
-    def getMinKey(self) -> str:
-        if self.prev_operation == 2:
-            return self.result
-        self.prev_operation = 2
-        if self.data:
-            count = list(self.data.values())[0]
-            key = list(self.data.keys())[0]
-            for key_d in self.data:
-                if self.data[key_d] < count:
-                    count = self.data[key_d]
-                    key = key_d
-            self.result = key
-            return key
-        else:
-            self.result = ""
-            return ""
-        
+            prev_node.keys.add(key)
+            self.data[key] = prev_node
+          
+        if not freq_node.keys:
+            self.removeNode(freq_node)
+
+        return None 
+
+    def getMaxKey(self) -> str:  
+        if self.tail.prev == self.head:
+            return '' 
+        return next(iter(self.tail.prev.keys))
+
+    def getMinKey(self) -> str: 
+        if self.head.next == self.tail:
+            return ''
+        return next(iter(self.head.next.keys))
+    
+    def removeNode(self, node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
         
 
 
