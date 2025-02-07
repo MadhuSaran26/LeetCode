655063f7f689c67d6df9c306d9d7e6fc1d519125
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-02-06 22:07:25 -0700

    Time: 11 ms (80.64%) | Memory: 18.2 MB (34.68%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -2,15 +2,20 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n=len(nums)
         # dp to hold the longest subsequence length until i
-        dp = [1]*n
-        
-        for i in range(1,n):
-            #checking all indexes before i
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    #get the maximum len considering subseq ending at j
-                    dp[i] = max(dp[i], dp[j]+1)
+        sub = [nums[0]]
 
-        return max(dp)
+        for idx in range(1,n):
+            num = nums[idx]
+            # if curr index value is greater than the largest element in subsequence, then extend it
+            if num > sub[-1]:
+                sub.append(num)
+            else:
+                i = 0
+                # finding the first element that is larger than or equal to num to replace it with num
+                while num > sub[i]:
+                    i += 1
+                sub[i] = num
+
+        return len(sub)
 
         
