4f1eab383b11e72df56c5f7b1006c1de78f377be
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-02-07 12:03:37 -0700

    Time: 0 ms (100.00%) | Memory: 18.8 MB (37.28%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.py
+++ 665-non-decreasing-array/non-decreasing-array.py
@@ -6,7 +6,7 @@ class Solution:
                 count -= 1
                 if count < 0:
                     return False
-                if idx < 2 or nums[idx-2] <= nums[idx]:
+                if idx == 1 or nums[idx-2] <= nums[idx]:
                     nums[idx-1] = nums[idx]
                 else:
                     nums[idx] = nums[idx-1]
