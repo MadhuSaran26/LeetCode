9f5e43b971399d3b74e24a552e9cc9a82d6fc725
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-02-06 17:40:02 -0700

    Time: 27 ms (30.75%) | Memory: 19.3 MB (36.06%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,23 +1,26 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
         n = len(height)
         result = 0
 
         # [3, 4, 1, 2, 2, 5, 1, 0, 2]
         #calculating max values from right until that index
         right_max = [0]*n
+        right_max[n-1] = height[n-1]
         for idx in range(n-2, -1, -1):
-            right_max[idx] = max(height[idx+1], right_max[idx+1])
+            right_max[idx] = max(height[idx], right_max[idx+1])
         
         #calculating max values from left until that index
         left_max = [0]*n
+        left_max[0] = height[0]
         for idx in range(1, n):
-            left_max[idx] = max(height[idx-1], left_max[idx-1])
+            left_max[idx] = max(height[idx], left_max[idx-1])
         
         for idx in range(n):
             #calculating hold capacity
-            capacity = min(left_max[idx], right_max[idx])
-            result += capacity - height[idx] if capacity - height[idx] > 0 else 0
+            result += min(left_max[idx], right_max[idx]) - height[idx]
         
         return result
 
