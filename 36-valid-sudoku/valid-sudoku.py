158a451530c28996938211ce952fe5d0279e8b5c
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-02-08 10:54:45 -0700

    Time: 7 ms (39.57%) | Memory: 17.7 MB (62.78%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -3,40 +3,30 @@ class Solution:
         row_set = [set() for _ in range(9)]
         col_set = [set() for _ in range(9)]
         box_set = [set() for _ in range(9)]
-        result = True
         for i in range(9):
             for j in range(9):
                 # element considered for check
                 elem = board[i][j]
+                # ignore empty cells
+                if elem == ".":
+                    continue
                 # check the row
                 if elem in row_set[i]:
-                    if elem != ".":
-                        result = False
-                        break
-                    else:
-                        continue
+                    return False
                 row_set[i].add(elem)
                 # check the column
                 if elem in col_set[j]:
-                    if elem != ".":
-                        result = False
-                        break
-                    else:
-                        continue
+                    return False
                 col_set[j].add(elem)
                 # check the box
                 box_r = i//3
                 box_c = j//3
                 box_id = box_r*3 + box_c
                 if elem in box_set[box_id]:
-                    if elem != ".":
-                        result = False
-                        break
-                    else:
-                        continue
+                    return False
                 box_set[box_id].add(elem)
             
-        return result
+        return True
 
 
 
