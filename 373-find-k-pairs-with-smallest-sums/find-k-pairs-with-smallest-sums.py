de03d13e57dd6c6fe15d57b7d4a686d3c5b214ef
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-02-11 23:22:45 -0700

    Time: 106 ms (51.52%) | Memory: 35 MB (41.78%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
@@ -3,6 +3,7 @@ class Solution:
         m, n = len(nums1), len(nums2)
         # since both arrays are sorted, the sum of first elements from these two arrays will be the smallest
         heap = [((nums1[0] + nums2[0]), 0, 0)]
+        visited = {(0,0)}
         result = []
         while heap:
             _, idx1, idx2 = heappop(heap)
@@ -12,12 +13,12 @@ class Solution:
                 return result
             
             # next possible smallest elements
-            if idx1+1 < m and idx2 == 0: # and (idx1+1, idx2) not in visited:
+            if idx1+1 < m and (idx1+1, idx2) not in visited:
                 heappush(heap, ((nums1[idx1+1] + nums2[idx2]), idx1+1, idx2))
-                #visited.add((idx1+1, idx2))
-            if idx2+1 < n: # and (idx1, idx2+1) not in visited:
+                visited.add((idx1+1, idx2))
+            if idx2+1 < n and (idx1, idx2+1) not in visited:
                 heappush(heap, ((nums1[idx1] + nums2[idx2+1]), idx1, idx2+1))
-                #visited.add((idx1, idx2+1))
+                visited.add((idx1, idx2+1))
         
         return result # when k > m*n
         
