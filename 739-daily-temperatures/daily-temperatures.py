eaaa2cf23153c0ff7cb1766d588c0594461da3f1
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-02-11 23:37:56 -0700

    Time: 86 ms (83.95%) | Memory: 27.5 MB (55.75%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -2,7 +2,7 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
         result = [0] * n
-        stack = []
+        stack = [] #to hold the temperatures for which warmer days are yet to be seen
 
         for curr_day in range(n):
             while stack and temperatures[stack[-1]] < temperatures[curr_day]:
