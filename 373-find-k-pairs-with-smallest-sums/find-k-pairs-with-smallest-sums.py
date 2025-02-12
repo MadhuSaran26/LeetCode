0f6f56782bcda2b3300c29f4d936325b9e530ced
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-02-11 23:23:28 -0700

    Time: 80 ms (88.47%) | Memory: 35 MB (41.78%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
@@ -4,21 +4,23 @@ class Solution:
         # since both arrays are sorted, the sum of first elements from these two arrays will be the smallest
         heap = [((nums1[0] + nums2[0]), 0, 0)]
         visited = {(0,0)}
-        result = []
+        result, result_len = [], 0
         while heap:
             _, idx1, idx2 = heappop(heap)
             result.append([nums1[idx1], nums2[idx2]])
-            k -= 1
-            if k == 0:
+            result_len += 1
+            if result_len == k:
                 return result
             
+            nidx1 = idx1+1 # (nidx1, idx2) 
+            nidx2 = idx2+1 # (idx1, nidx2)
             # next possible smallest elements
-            if idx1+1 < m and (idx1+1, idx2) not in visited:
-                heappush(heap, ((nums1[idx1+1] + nums2[idx2]), idx1+1, idx2))
-                visited.add((idx1+1, idx2))
-            if idx2+1 < n and (idx1, idx2+1) not in visited:
-                heappush(heap, ((nums1[idx1] + nums2[idx2+1]), idx1, idx2+1))
-                visited.add((idx1, idx2+1))
+            if nidx1 < m and (nidx1, idx2) not in visited:
+                heappush(heap, ((nums1[nidx1] + nums2[idx2]), nidx1, idx2))
+                visited.add((nidx1, idx2))
+            if nidx2 < n and (idx1, nidx2) not in visited:
+                heappush(heap, ((nums1[idx1] + nums2[nidx2]), idx1, nidx2))
+                visited.add((idx1, nidx2))
         
         return result # when k > m*n
         
