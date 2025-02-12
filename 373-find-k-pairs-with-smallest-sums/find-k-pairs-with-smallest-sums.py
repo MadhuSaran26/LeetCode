b85416153ce436484497cdb5c78fb40e63905c8d
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-02-11 23:10:24 -0700

    Time: 99 ms (61.98%) | Memory: 35 MB (41.78%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
@@ -1,27 +1,23 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
         m, n = len(nums1), len(nums2)
+        # since both arrays are sorted, the sum of first elements from these two arrays will be the smallest
+        heap = [((nums1[0] + nums2[0]), 0, 0)]
+        visited = {(0,0)}
         result = []
-        len_result = 0
-        # since both nums1 and nums2 are sorted, sum of the first elements from both will be the smallest
-        heap = [(nums1[0]+nums2[0], 0, 0)]
-        visited = set()
-
         while heap:
             _, idx1, idx2 = heappop(heap)
             result.append([nums1[idx1], nums2[idx2]])
-            len_result += 1
-            if len_result == k:
+            if len(result) == k:
                 return result
-
-            if (idx1, idx2+1) not in visited and idx2+1 < n:
-                heappush(heap, (nums1[idx1]+nums2[idx2+1], idx1, idx2+1))
-                visited.add((idx1, idx2+1))
-            if (idx1+1, idx2) not in visited and idx1+1 < m:
-                heappush(heap, (nums1[idx1+1]+nums2[idx2], idx1+1, idx2))
+            
+            # next possible smallest elements
+            if idx1+1 < m and (idx1+1, idx2) not in visited:
+                heappush(heap, ((nums1[idx1+1] + nums2[idx2]), idx1+1, idx2))
                 visited.add((idx1+1, idx2))
-            
-        return result #when k value is greater than m*n
+            if idx2+1 < n and (idx1, idx2+1) not in visited:
+                heappush(heap, ((nums1[idx1] + nums2[idx2+1]), idx1, idx2+1))
+                visited.add((idx1, idx2+1))
         
-
-            
+        return result # when k > m*n
+        
