f7608a72618b945bbcb853769608c78c8e4440f9
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-03-02 23:42:53 -0700

    Time: 0 ms (100.00%) | Memory: 18.2 MB (9.32%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        memo = {}
+
+        def helper(i):
+            if i in memo:
+                return memo[i]
+            if i==n:
+                return 1
+            if s[i]=="0":
+                return 0
+            
+            ways = helper(i+1)
+
+            if i != n-1 and s[i:i+2] <= "26":
+                ways += helper(i+2)
+            
+            memo[i] = ways
+            return ways
+        
+        return helper(0)
