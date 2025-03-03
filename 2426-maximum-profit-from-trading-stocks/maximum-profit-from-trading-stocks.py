99d8d4026b284ec74d36ff5b36720c570b878cda
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-03-03 13:51:42 -0700

    Time: 1384 ms (48.52%) | Memory: 18 MB (63.31%) - LeetSync

--- 2426-maximum-profit-from-trading-stocks/maximum-profit-from-trading-stocks.py
+++ 2426-maximum-profit-from-trading-stocks/maximum-profit-from-trading-stocks.py
@@ -2,13 +2,24 @@ class Solution:
     def maximumProfit(self, present: List[int], future: List[int], budget: int) -> int:
         n = len(present)
         # number of items (rows), remaining capacity (cols)
-        dp = [[0 for _ in range(budget+1)] for _ in range(n+1)] 
+        curr = [0 for _ in range(budget+1)]
 
-        for i in range(1,n+1):
+        for i in range(1, n+1):
+            prev = curr[:]
             for j in range(budget+1):
                 if present[i-1] <= j:
-                    dp[i][j] = max(dp[i-1][j], dp[i-1][j - present[i-1]] + (future[i-1] - present[i-1]))
+                    curr[j] = max(prev[j], prev[j - present[i-1]] + (future[i-1] - present[i-1]))
                 else:
-                    dp[i][j] = dp[i-1][j]
-
-        return dp[n][budget]    
+                    curr[j] = prev[j]
+        
+        return curr[budget]
+        ## dp = [[0 for _ in range(budget+1)] for _ in range(n+1)] 
+## 
+        ## for i in range(1,n+1):
+        ##     for j in range(budget+1):
+        ##         if present[i-1] <= j:
+        ##             dp[i][j] = max(dp[i-1][j], dp[i-1][j - present[i-1]] + (future[i-1] - present[i-1]))
+        ##         else:
+        ##             dp[i][j] = dp[i-1][j]
+## 
+        ## return dp[n][budget]    
