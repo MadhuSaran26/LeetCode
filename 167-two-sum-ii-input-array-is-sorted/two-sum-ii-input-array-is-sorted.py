06e7cbad69e4cc1e2feaf8eb7511c840d8f67e39
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-08-02 18:52:23 -0700

    Time: 3 ms (80.90%) | Memory: 18.6 MB (59.39%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,13 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        sum_pair = dict()
-        result = []
-        for idx, num in enumerate(numbers):
-            if target - num in sum_pair:
-                result = [sum_pair[target-num], idx+1]
-            sum_pair[num] = idx+1
-        return result
+        left, right = 0, len(numbers)-1
+        while right > left:
+            current_sum = numbers[left] + numbers[right]
+            if current_sum == target:
+                return [left+1, right+1]
+            elif current_sum > target:
+                right -= 1
+            else:
+                left += 1
+            
+                
