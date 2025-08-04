0040e13be0c25aaf8c0d8d8cc2bdbd64117284a0
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-08-03 20:35:29 -0700

    Time: 76 ms (53.10%) | Memory: 18.3 MB (54.43%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,28 +1,26 @@
+from collections import defaultdict, Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         left = 0
-        s = list(s)
-        slen = len(s)
-        tcnt = defaultdict(int)
-        for c in t:
-            tcnt[c] += 1
-        window_cnt = defaultdict(int)
-        min_window = (float('inf'), -1, -1)
+        tcnt = Counter(t)
         have, need = 0, len(tcnt)
-        for right in range(slen):
+        window_cnt = defaultdict(int)
+        min_window = [float('inf'), -1, -1]
+        for right in range(len(s)):
             window_cnt[s[right]] += 1
             if s[right] in tcnt and window_cnt[s[right]] == tcnt[s[right]]:
                 have += 1
+            
             while have == need:
-                if min_window[0] > right-left+1:
-                    min_window = (right-left+1, left, right)
-               
+                if min_window[0] > right - left + 1:
+                    min_window = [right - left + 1, left, right]
+                
                 window_cnt[s[left]] -= 1
                 if s[left] in tcnt and window_cnt[s[left]] < tcnt[s[left]]:
                     have -= 1
                 left += 1
-
-        return ''.join(s[min_window[1]:min_window[2]+1]) if min_window[0] != float('inf') else ''
+        
+        return s[min_window[1]:min_window[2]+1] if min_window[0] != float('inf') else ''
 
 
 
