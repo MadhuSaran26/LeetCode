0ecfe8b92e02d513a0e2c47797748cb714a98e77
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-06 20:37:20 -0700

    Time: 66 ms (61.60%) | Memory: 21.9 MB (25.83%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        n = len(s)
+        current = 0
+        operation = '+'
+        stack = []
+        for i, char in enumerate(s):
+            if char.isdigit():
+                current = current * 10 + int(char)
+            if char in {'+','-','*','/'} or i == n-1:
+                if operation == '+':
+                    stack.append(current)
+                if operation == '-':
+                    stack.append(-current)
+                if operation == '*':
+                    stack.append(stack.pop() * current)
+                if operation == '/':
+                    stack.append(int(stack.pop() / current))
+                operation = char
+                current = 0
+        return sum(stack)
+
+            
+
+
+
+
+
+        
