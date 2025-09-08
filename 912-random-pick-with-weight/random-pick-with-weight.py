f540779100ecaf6a6274438bb899c817ce57bab2
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-07 23:52:20 -0700

    Time: 99 ms (27.57%) | Memory: 23.5 MB (48.79%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -2,15 +2,24 @@ import random
 class Solution:
 
     def __init__(self, w: List[int]):
-        w_sum = sum(w)
-        self.weights = [num/w_sum for num in w]
-        self.indices = list(range(len(w)))
-        print(self.weights)
-        print(self.indices)
+        self.prefix_sum = [0] * len(w)
+        cum_sum = 0
+        for idx, num in enumerate(w):
+            cum_sum += num
+            self.prefix_sum[idx] = cum_sum
+            
 
     def pickIndex(self) -> int:
-        result = random.choices(self.indices, weights=self.weights, k=1)
-        return result[0]
+        target = random.randint(1, self.prefix_sum[-1])
+        low, high = 0, len(self.prefix_sum)-1
+
+        while low <= high:
+            mid = (low + high)//2
+            if self.prefix_sum[mid] >= target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
         
 
 
