a4879b7965df72df939b039717b965fa39d635e5
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-17 21:24:39 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (50.18%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if x == 0:
+            return 0
+        if n < 0:
+            return self.myPow(1/x, -n)
+        # Check for even number
+        if (n^1) == (n+1):
+            return self.myPow(x*x, n//2)
+        else:
+            return x * self.myPow(x*x, n//2)
+        
+        
