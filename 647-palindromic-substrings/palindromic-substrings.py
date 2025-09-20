019df95b61460208be1eda4ecaed9a72a646aa8f
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-20 12:24:55 -0700

    Time: 7 ms (98.80%) | Memory: 18.1 MB (26.21%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,20 +1,28 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        s = "^#" + "#".join(s) + "#$" # convert s of odd length (2n + 1)
+        radii = [0] * len(s)
+        C = R = 0 # variables to track the longest palindrome center and the boundary so far
         result = 0
-
-        def countPalindrome(start, end):
-            count = 0
-            while 0 <= start and end < len(s) and s[start] == s[end]:
-                start -= 1
-                end += 1
-                count += 1
-            return count
-        
-        for i in range(len(s)):
-            result += countPalindrome(i, i)
-            if i < len(s) - 1:
-                result += countPalindrome(i, i+1)
-        
+        for center in range(len(s)):
+            # if center is within the boundary of the longest palindrome so far
+            # the initial palidrome length around this center can be found using the mirror radius
+            # since the palindrome around mirror can extend beyond the left boundary, minimum is considered
+            if center < R:
+                mirror = 2*C - center
+                radii[center] = min(R-center, radii[mirror])
+            
+            # expand further to check whether palindrome length can be extended around center
+            while center + radii[center] + 1 < len(s) and center - radii[center] - 1 >= 0 and s[center + radii[center] + 1] == s[center - radii[center] - 1]:
+                radii[center] += 1
+            
+            # if the current palindrome extends beyond the previous longest palindrome's boundary
+            # then update the longest palindrome's C and R values
+            if center + radii[center] > R:
+                R = center + radii[center]
+                C = center
+            
+            # Every expansion results in palindromic substrings
+            result += (radii[center] + 1) // 2
+            
         return result
-
-        
