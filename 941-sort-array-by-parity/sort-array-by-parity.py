a60507b983fc050b089bc414229dc1396fdd5316
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-20 16:41:41 -0700

    Time: 3 ms (46.70%) | Memory: 18.2 MB (96.72%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -2,7 +2,7 @@ class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         nextEvenAt = 0
         for i in range(len(nums)):
-            if nums[i]^1 == nums[i]+1:
+            if nums[i]%2==0:
                 nums[i], nums[nextEvenAt] = nums[nextEvenAt], nums[i]
                 nextEvenAt += 1
         return nums
