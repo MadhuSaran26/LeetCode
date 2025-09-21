56d156a8f591b51c17d1846b984c0dccb4e8d3fb
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-21 00:36:11 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.10%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = len(nums) - 2
+        l = len(nums) - 1
+        
+        # iterate till the index where it is non-decreasing
+        while k >= 0 and nums[k] >= nums[k+1]:
+            k -= 1
+        
+        if k == -1:
+            nums.reverse()
+            return
+        
+        # iterate till the index of the number that is greater than nums[k]
+        while l >= 0 and nums[k] >= nums[l]:
+            l -= 1
+        
+        # swap nums[k] and nums[l] to form the next permutation in the lexographical order
+        nums[k], nums[l] = nums[l], nums[k]
+
+        nums[k+1:] = nums[k+1:][::-1]
+
+        
