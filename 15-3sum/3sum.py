2a6ddbb96b246c334043dac23dfadb5dca7727c2
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-21 14:05:17 -0700

    Time: 464 ms (84.58%) | Memory: 20.8 MB (19.20%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,27 +2,28 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         result = []
-
-        for fptr in range(len(nums)):
-            if fptr > 0 and nums[fptr] == nums[fptr-1]:
+        
+        for i in range(len(nums)):
+            # after i all are positive numbers, hence a target of 0 is unachievable
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            left, right = fptr + 1, len(nums)-1
+            target = - nums[i]
+            left, right = i+1, len(nums)-1
             while left < right:
-                total = nums[fptr] + nums[left] + nums[right]
-                if total < 0:
+                if nums[left] + nums[right] == target:
+                    result.append([nums[i], nums[left], nums[right]])
                     left += 1
-                elif total > 0:
                     right -= 1
-                else:
-                    result.append([nums[fptr], nums[left], nums[right]])
-                    
-                    while left < right and nums[left] == nums[left+1]:
+                    while nums[left] == nums[left - 1] and left < right:
                         left += 1
-                    while left < right and nums[right] == nums[right-1]:
-                        right -= 1
-                    
+                elif nums[left] + nums[right] < target:
                     left += 1
+                else:
                     right -= 1
-
+        
         return result
-            
+                
+
+        
