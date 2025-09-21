fc2f2e82ef37b34933b2f9721d19cfa6b059e786
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-21 13:30:29 -0700

    Time: 5 ms (41.87%) | Memory: 17.7 MB (97.40%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,31 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry_over = 0
+        carry = 0
         ptr1 = l1
         ptr2 = l2
         dummy = ListNode()
         ptr3 = dummy
-        while ptr1 and ptr2:
-            sum_val = ptr1.val + ptr2.val + carry_over
-            carry_over = sum_val // 10
+        while ptr1 or ptr2 or carry:
+            sum_val = carry
+            if ptr1:
+                sum_val += ptr1.val
+                ptr1 = ptr1.next
+            if ptr2:
+                sum_val += ptr2.val
+                ptr2 = ptr2.next
+            carry = sum_val // 10
             ptr3.next = ListNode(sum_val%10)
-            ptr1, ptr2, ptr3 = ptr1.next, ptr2.next, ptr3.next
-        
-        while ptr1:
-            sum_val = ptr1.val + carry_over
-            carry_over = sum_val // 10
-            ptr3.next = ListNode(sum_val%10)
-            ptr1, ptr3 = ptr1.next, ptr3.next
-        
-        while ptr2:
-            sum_val = ptr2.val + carry_over
-            carry_over = sum_val // 10
-            ptr3.next = ListNode(sum_val%10)
-            ptr2, ptr3 = ptr2.next, ptr3.next
-        
-        if carry_over:
-            ptr3.next = ListNode(carry_over)
+            ptr3 = ptr3.next
         
         return dummy.next
 
