6bdbaa1f90f7a021a27c750baa2b32686fa94f4a
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-22 09:27:19 -0700

    Time: 6 ms (76.17%) | Memory: 17.8 MB (92.23%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.py
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.py
@@ -3,17 +3,16 @@ class Solution:
         result = [0] * n
         stack = []
         for log in logs:
-            idx, etype, time = log.split(":")
-            idx, time = int(idx), int(time)
-            if etype == "start":
+            id_exec = log.split(":")
+            idx, time = int(id_exec[0]), int(id_exec[2])
+            if id_exec[1] == "start":
                 if stack:
-                    # pause current running function
                     result[stack[-1][0]] += time - stack[-1][1]
                 stack.append((idx, time))
             else:
                 if stack and stack[-1][0] == idx:
-                    fid, start_time = stack.pop()
-                    result[fid] += time - start_time + 1
+                    result[stack[-1][0]] += time - stack[-1][1] + 1
+                    stack.pop()
                     if stack:
                         stack[-1] = (stack[-1][0], time+1)
                 else:
