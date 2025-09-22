cd70647932714659f50f82c2aefed697999e7257
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-22 00:26:21 -0700

    Time: 3 ms (89.05%) | Memory: 17.9 MB (85.28%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -1,18 +1,17 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         n1, n2 = len(num1), len(num2)
-        p1 = p2 = 0
-        num1, num2 = num1[::-1], num2[::-1]
+        p1, p2 = n1-1, n2-1
         carry = 0
         result = ""
-        while p1 < n1 or p2 < n2 or carry:
+        while p1 >= 0 or p2 >= 0 or carry:
             csum = carry
-            if p1 < n1:
+            if p1 >= 0:
                 csum += int(num1[p1])
-                p1 += 1
-            if p2 < n2:
+                p1 -= 1
+            if p2 >= 0:
                 csum += int(num2[p2])
-                p2 += 1
+                p2 -= 1
             carry = csum // 10
             result += str(csum%10)
         return result[::-1]
