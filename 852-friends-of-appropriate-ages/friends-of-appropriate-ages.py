ae384f8696a9ad2ce4cda061ee4c4eae468537d4
Madhumitha Saravanan <117112319+MadhuSaran26@users.noreply.github.com>
2025-09-21 20:25:46 -0700

    Time: 87 ms (37.39%) | Memory: 18.3 MB (91.14%) - LeetSync

---
+++ 852-friends-of-appropriate-ages/friends-of-appropriate-ages.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numFriendRequests(self, ages: List[int]) -> int:
+        ages.sort()
+        result = 0
+        left = right = 0
+        for i, age in enumerate(ages):
+            if age < 15:
+                continue
+            while ages[left] <= age * 0.5 + 7:
+                left += 1
+            while right + 1 < len(ages) and ages[right + 1] <= age:
+                right += 1
+            result += right - left
+        return result
